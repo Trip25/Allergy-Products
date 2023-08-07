97b749442a02ece12ede603a2720ddf006e9028a
Trip25 <t.gurung1995@gmail.com>
2023-08-07 11:07:52 +0100

    added plan for filter and dl bootstrap

--- src/Components/FilterMenu/index.js
+++ src/Components/FilterMenu/index.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+/*Plan
+Filter for allergy 
+so it will go through "diet" in products.json
+use checkbox to select 
+if checked, we want to show products that satisfy that diet
+set an allergy array
+
+display in div
+product.name
+product.description
+product.price
+product.diet (show all that is checked)
+*/
 
 export default function FilterMenu() {
+    const []
     
         return (
             <div>
