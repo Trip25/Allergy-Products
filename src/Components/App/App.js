3aa39086ff78ed1048bc2c7f23d773a21cf88ee6
pasujemito <damian.janik@iceland.co.uk>
2023-08-07 12:29:12 +0100

    adds navigati

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,26 @@
 import './App.css'
-import ProductList from '../ProductList'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+import ProductList from '../ProductList'
+import FilterMenu from '../FilterMenu';
+
+const LogoNavigation = () => {
+  return (
+    <Navbar className='border-bottom'>
+    <Container className="d-flex justify-content-center">
+      <Navbar.Brand href="#home">Allergy App </Navbar.Brand>
+    </Container>
+  </Navbar>
+  );
+};
 
 function App() {
   return (
     <div className="App">
+      <LogoNavigation/ >
+      <FilterMenu/>
       <ProductList />
     </div>
   )
