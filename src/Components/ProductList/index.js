7062f40f4dd31d7a3b5ff705c10fcf586bb47977
NeilBrooks <neil.brooks87@gmail.com>
2023-08-07 11:58:37 +0100

    Added cards with images

--- src/Components/ProductList/index.js
+++ src/Components/ProductList/index.js
@@ -1,34 +1,33 @@
-import { useState } from 'react';
-import Products from '../../products.json';
 import { Badge, Card } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Products from '../../products.json';
 
-const ProductCard = ({name, desc, price, diet})=> {
+const ProductCard = ({ image, name, desc, price, diet }) => {
   return (
-      <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>{name}</Card.Title>
-            <Card.Text>{desc}</Card.Text>
-            <Card.Text>{price}</Card.Text>
-            {diet.map((title, index) => <Badge key={index}>{title}</Badge>)}
-          </Card.Body>
-        </Card>
-  )
-}
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Img variant="top" src={image} style={{width: '200px', height: '200px'}} />
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{desc}</Card.Text>
+        <Card.Text>{price}</Card.Text>
+        {diet.map((title, index) => <Badge key={index}>{title}</Badge>)}
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default function ProductList() {
-
-        return (
-            <ul>
-                {Products.map(({name, description, price, diet}, index) => {
-                    return (
-                        <li key={index}>
-                            <ProductCard name={name} desc={description} price={price} diet={diet} />
-                        </li>
-                    )
-                })}
-            </ul>
-
-        );
-      }
-    
-    
+  return (
+    <Container fluid>
+      <Row>
+        {Products.map(({ image, name, description, price, diet }, index) => (
+          <Col style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}} key={index} md={3}>
+            <ProductCard image={image} name={name} desc={description} price={price} diet={diet} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
